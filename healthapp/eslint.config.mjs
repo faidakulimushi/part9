16b33781b1535fdc175a8a3f1654138dabc9c7e1
faidakulimushi <faidakulimushi431@gmail.com>
2026-08-05 12:33:04 +0300

    Complete exercise 9.6

--- healthapp/eslint.config.mjs
+++ healthapp/eslint.config.mjs
@@ -6,16 +6,16 @@ export default tseslint.config({
   files: ["**/*.ts"],
   extends: [
     eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked
+    ...tseslint.configs.recommendedTypeChecked,
   ],
   languageOptions: {
     parserOptions: {
       project: true,
-      tsconfigRootDir: import.meta.dirname
-    }
+      tsconfigRootDir: import.meta.dirname,
+    },
   },
   plugins: {
-    "@stylistic": stylistic
+    "@stylistic": stylistic,
   },
   rules: {
     "@stylistic/semi": "error",
